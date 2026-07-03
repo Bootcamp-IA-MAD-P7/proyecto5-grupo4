4fab187db6df8480236b347543cf86b92e27b409
anahi-am <anahiamerisea@gmail.com>
2026-07-03 14:44:06 +0200

    chore: project setup and add dataset merge

---
+++ eda/full_eda.py
@@ -0,0 +1,35 @@
+# %%
+import polars as pl
+import altair as alt
+import pyarrow as pa
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+
+alt.data_transformers.enable("vegafusion")
+plt.style.use("seaborn-v0_8-whitegrid")
+
+df = pl.read_parquet("../data/processed/edos_merged.parquet")
+df_pd = df.to_pandas()
+
+
+# %%
+
+
+# %%
+"""
+Code for saving this file as a notebook.
+# It Goes at the end of the file.
+"""
+# %%
+if __name__ == "__main__":
+    import jupytext as jpt
+
+    _py = Path(__file__)
+    _nb = _py.with_suffix(".ipynb")
+    if not _nb.exists():
+        jpt.write(jpt.read(_py), _nb, fmt="ipynb")
+        print(f"Created {_nb.name}")
+    else:
+        jpt.write(jpt.read(_py), _nb, fmt="ipynb")
+        print(f"Synced {_nb.name}")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from proyecto5-grupo4!")
+
+
+if __name__ == "__main__":
+    main()

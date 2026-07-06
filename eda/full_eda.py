e1c7a6cc0eef07f6b945eeb5e010b95372433635
anahi-am <anahiamerisea@gmail.com>
2026-07-06 11:12:21 +0200

    feature: add outliers graphs

--- eda/full_eda.py
+++ eda/full_eda.py
@@ -4,6 +4,7 @@ import altair as alt
 import pyarrow as pa
 import matplotlib.pyplot as plt
 import seaborn as sns
+import vegafusion
 import os
 from pathlib import Path
 
@@ -15,12 +16,55 @@ try:
 except NameError:
     _base = Path(os.getcwd()).parent
 df = pl.read_parquet(_base / "data" / "processed" / "merged_edos_sexism.parquet")
-df_pd = df.to_pandas()
+df_pd = df.to_pandas().reset_index()
 
-
+"""
+ML-17. Outlier analysis
+"""
 # %%
 
+box = (
+    alt.Chart(df_pd)
+    .mark_boxplot(color="steelblue")
+    .encode(y=alt.Y("toxicity", title="Toxicity Score"))
+    .properties(title="Boxplot - Toxicity", width=200)
+)
 
+hist = (
+    alt.Chart(df_pd)
+    .mark_bar()
+    .encode(
+        x=alt.X("toxicity", bin=True, title="Toxicity Score"),
+        y=alt.Y("count()", title="Count"),
+        color=alt.Color("toxicity", bin=True, scale=alt.Scale(scheme="rainbow")),
+    )
+    .properties(title="Distribution - Toxicity", width=400)
+)
+
+scatter = (
+    alt.Chart(df_pd)
+    .mark_circle(opacity=0.4, size=30)
+    .encode(
+        x=alt.X("index:Q", title="Index"),
+        y=alt.Y("toxicity", title="Toxicity Score"),
+        color=alt.Color("toxicity", scale=alt.Scale(scheme="rainbow")),
+    )
+    .properties(title="Scatter - Toxicity", width=400)
+)
+
+(box | hist).resolve_scale(color="independent").display()
+scatter.display()
+
+# %%
+scatter = (
+    alt.Chart(df_pd)
+    .mark_rect()
+    .encode(
+        x=alt.X("index:Q", bin=alt.Bin(maxbins=50)),
+        y=alt.Y("toxicity", bin=alt.Bin(maxbins=30)),
+        color=alt.Color("count()", scale=alt.Scale(scheme="rainbow")),
+    )
+).show()
 # %%
 """
 Code for saving this file as a notebook.

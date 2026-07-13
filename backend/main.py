4853f009f02da6b7975b8a10e605c14449cf4fdb
karinaromerovasquez <karinaromerovasquez6@gmail.com>
2026-07-13 13:04:10 +0200

    feat: add CORS, fix Dockerfile PORT, add requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from scripts.predict import predict_sexism
 
 app = FastAPI(title="Sexism Detector API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://proyecto5-grupo4.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class PredictInput(BaseModel):
